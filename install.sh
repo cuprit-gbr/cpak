a977cec176a8d996e895631bf4fb5ca3f61df362
Toni Schönbuchner <toni.schoenbuchner@csgis.de>
2021-11-22 17:34:45 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ mkdir -p $PROJECT_DIRECTORY
 curl -L "https://github.com/cuprit-gbr/cpak/releases/download/$VERSION/cpak" -o $PROJECT_DIRECTORY/cpak
 chmod +x $PROJECT_DIRECTORY/cpak
 ln -s $PROJECT_DIRECTORY/cpak /usr/local/bin/cpak
+
+echo "created folder in $PROJECT_DIRECTORY"
+echo "symlink created /usr/local/bin/cpak"
+echo "try to run the script with cpak"
